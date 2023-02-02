6982161b202e197f25048f9b534c00675f1ed694
m1rade <forx98@gmail.com>
2023-02-02 22:58:39 +0300

    Super sort

--- src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react"
 
 // добавить в проект иконки и импортировать
-const downIcon = '[\\/]'
-const upIcon = '[/\\]'
-const noneIcon = '[--]'
+const downIcon = "[\\/]"
+const upIcon = "[/\\]"
+const noneIcon = "[--]"
 
 export type SuperSortPropsType = {
     id?: string
@@ -14,16 +14,23 @@ export type SuperSortPropsType = {
 
 export const pureChange = (sort: string, down: string, up: string) => {
     // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
-    return up // исправить
+    switch (sort) {
+        case down:
+            return up;
+        case up:
+            return "";
+        default:
+            return down;
+    }
 }
 
 const SuperSort: React.FC<SuperSortPropsType> = (
     {
-        sort, value, onChange, id = 'hw15',
+        sort, value, onChange, id = "hw15",
     }
 ) => {
-    const up = '0' + value
-    const down = '1' + value
+    const up = "0" + value
+    const down = "1" + value
 
     const onChangeCallback = () => {
         onChange(pureChange(sort, down, up))
@@ -37,7 +44,7 @@ const SuperSort: React.FC<SuperSortPropsType> = (
 
     return (
         <span
-            id={id + '-sort-' + value}
+            id={id + "-sort-" + value}
             onClick={onChangeCallback}
         >
             {/*сделать иконку*/}
